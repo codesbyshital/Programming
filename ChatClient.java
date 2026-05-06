5f6d86120c8d8fd3dd31a0a8fa4b36fae3e5f2c0
codesbyshital <smurumkar20@gmail.com>
2026-05-06 12:16:11 +0530

    Client-Server Architecture by Socket programming
    This includes client-server communication using Socket Programming.
    includes concept of IP, Port, Socket & BufferedReader, InputStreamReader, getOutputStream, PrintStream, etc.

---
+++ ChatClient.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.net.*;
+
+
+class ChatClient
+{
+    public static void main(String A[]) throws Exception
+    {
+        System.out.println("Client Appliction is Running.... ");
+
+        Socket sobj = new Socket("localhost", 2100);    //connection
+
+        System.out.println("Connection is Successfull with Server");
+
+        PrintStream pobj = new PrintStream(sobj.getOutputStream());
+
+        BufferedReader bobj1 = new BufferedReader(new InputStreamReader(sobj.getInputStream()));
+
+        BufferedReader bobj2 = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("------------Marvellous Chat Client----------------");
+        System.out.println("--------------------------------------------------");
+
+        String str1 = null , str2 = null;
+
+        System.out.println("Enter Message for server");
+
+        while(!(str1 = bobj2.readLine()).equals("end"))
+        {
+            pobj.println(str1);
+            str2 = bobj1.readLine();
+
+            System.out.println("Server Says"+str2);
+            System.out.println("Enter Message for server");
+        }
+
+        sobj.close();
+                    
+
+    }
+}
+
+
+
+
+
+
+
