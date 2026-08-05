3bdfedf38011d67325096af46598493fb20de8f5
codesbyshital <smurumkar20@gmail.com>
2026-08-05 16:13:49 +0530

    EduTrack – Classroom & Student Management Portal
    •	Developed backend services for managing student records and batch operations. 
    •	Implemented RESTful CRUD APIs with NoSQL database integration.
    •	Designed modular architecture for scalability and maintainability.

---
+++ Java_Programming/Projects/MarvellousPortalFullStackResponseEntity/src/main/java/com/marvellous/MarvellousPortal/Entity/BatchEntry.java
@@ -0,0 +1,17 @@
+package com.marvellous.MarvellousPortal.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "BatchDetails")
+@Getter
+@Setter
+
+public class BatchEntry
+{
+    private String id;
+    private String name;
+    private int fees;
+}
